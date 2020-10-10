9b30fa9693460e9fba479ede7dd486e6dd34641f
franky_232003@yahoo.com <franky_232003@yahoo.com>
2020-10-09 20:43:46 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            
+            int b = 4; 
+       
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+        }
+    }
+}
